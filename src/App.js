2dda3ed4f4d5cdaf3111202ab24b35b84d2addf0
ojdarez <ojdarez@gmail.com>
2021-08-11 04:29:51 -0400

    Organized App.js as component.
    Using dynamically loaded JSON data for Menu links in Header component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in less than a day!!!
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header.js'
+import Footer from './components/Footer.js'
+import Main from './components/Main';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        
+        <Main />
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
